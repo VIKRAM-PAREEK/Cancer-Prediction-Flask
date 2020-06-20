6db0c703e8ee4eb183044fa070f625d097378acc
VIKRAM-PAREEK <vikrampareek920@gmail.com>
2020-06-20 14:03:33 +0530

    form with required input

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods= ['GET','POST'])
 def home():
-   return render_template('frontpage.html')
+	name = ''
+	if request.method=='POST' and 'username' in request.form:
+		name = request.form.get('username')
+	return render_template('frontpage.html',name=name)
 
 
 app.run(debug = True)
