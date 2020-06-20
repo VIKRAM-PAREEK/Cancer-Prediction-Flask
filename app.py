f2bea4b4fec5d5326def7fd1f30009816a84a78e
VIKRAM-PAREEK <vikrampareek920@gmail.com>
2020-06-20 19:36:35 +0530

    sending data from frontpage to results page

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route('/',methods= ['GET','POST'])
-def home():
-	name = ''
-	if request.method=='POST' and 'username' in request.form:
-		name = request.form.get('username')
-	return render_template('frontpage.html',name=name)
+def home():	
+	return render_template('frontpage.html',name=name,val=val)
 
+@app.route('/results', methods= ['GET','POST'])
+
+def result():
+	name = ''
+	val = ''
+	if request.method == 'POST':
+ 		val = request.form
+ 		name = request.form.get('username')
+	return render_template('results.html',name = name)
 
 app.run(debug = True)
